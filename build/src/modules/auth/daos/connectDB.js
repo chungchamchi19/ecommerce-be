092cad2d9796b28d6bc2bb6f80cb27ac962c084a
Chung Cham Chi <dinhchungkhtnk29@gmail.com>
2021-10-24 00:03:37 +0700

    fix: read me

--- build/src/modules/auth/daos/connectDB.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
-const connectDB = typeorm_1.createConnection();
-exports.default = connectDB;
