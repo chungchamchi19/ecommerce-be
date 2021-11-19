02f5e68a00a0d0aef34726f43baab6191f44e49c
phamhungicthust <phamhungicthust@gmail.com>
2021-11-20 01:02:33 +0700

    feat: first add product_collection

---
+++ src/modules/productCollection/controllers.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import productCollectionServices from "./services";
+import { ProductCollection } from "../../entities/productCollection";
+
+const createProductCollection = async (req: Request, res: Response) => {
+}
